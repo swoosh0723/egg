81fd4f0c920c6d1388e1b24f34996f8019f40e68
Kang Hyeonseok <delantero0723@naver.com>
2022-01-24 20:48:09 +0900

    [STUDY-20220124] jsx란? 데이터바인딩이란?

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let posts = "강남 고기 맛집";
+  let style = { color: "pink", fontSize: "30px" };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={style}>개발 blog</div>
+      </div>
+      <h4>{posts}</h4>
+      <img src={logo} alt="logo"></img>
     </div>
   );
 }
 
 export default App;
 
-// test2
-// PR
+// react 가장 큰 장점
+// 데이터 바인딩이 쉽다
+// 데이터 바인딩이란?
+// api로 넘어오는 데이터를 변수로 넣는다.
+// 변수 이미지 함수 스타일 등등 .... 쉽게 넣을 수 있다
+
+// style은 중괄호 object 형식으로
