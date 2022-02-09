83b62d5fd9a3de99d3e494be41a63c7e43dcf653
Kang Hyeonseok <delantero0723@naver.com>
2022-02-09 15:38:16 +0900

    [STUDY-20210209] props 사용하여 데이터바인딩

--- src/App.js
+++ src/App.js
@@ -5,25 +5,35 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-
+  let [title, titleChange] = useState([
+    '성수동 맛집',
+    '강남 맛집',
+    '제주도 맛집'
+  ]);
 
   return (
     <div className="App">
       <div className="black-nav">학습내용 복습</div>
       <div className="list">
-        <ListItem></ListItem>
+
+        <ListItem
+          title={title}
+        ></ListItem>
       </div>
     </div>
+
+
   );
 
 }
 
 // components명은 반드시 대문자
-function ListItem() {
+// pros를 받을때는 사용한 곳에 바인딩 되어야함
+function ListItem(props) {
   return (
     <>
       <div className="listItem">
-        <h3>글제목</h3>
+        <h3>{props.title[0]}</h3>
         <p>날짜: 2월 1일</p>
       </div>
     </>
