7c40e3a26861ece24570f25dd08c94f38ce46427
Kang Hyeonseok <delantero0723@naver.com>
2022-02-09 15:53:39 +0900

    [STUDY-20210209] 좋아요 버튼 생성

--- src/App.js
+++ src/App.js
@@ -11,13 +11,16 @@ function App() {
     '제주도 맛집'
   ]);
 
+  let [likeCount, likeCountChange] = useState(0)
+
   return (
     <div className="App">
       <div className="black-nav">학습내용 복습</div>
       <div className="list">
-
         <ListItem
           title={title}
+          likeCount={likeCount}
+          likeCountChange={likeCountChange}
         ></ListItem>
       </div>
     </div>
@@ -27,13 +30,25 @@ function App() {
 
 }
 
+// 1. components 생성
 // components명은 반드시 대문자
 // pros를 받을때는 사용한 곳에 바인딩 되어야함
+// useState 변경할때는 Setter를 변경해주어야함
+// let [현재상태, setter] = useState(0);
 function ListItem(props) {
   return (
     <>
       <div className="listItem">
         <h3>{props.title[0]}</h3>
+        <button
+          type="button"
+          onClick={() => { props.likeCountChange(props.likeCount + 1) }}
+        >
+          좋아요
+        </button>
+        <span>
+          {props.likeCount}
+        </span>
         <p>날짜: 2월 1일</p>
       </div>
     </>
